485e6104bf3656c0dc8cd15bdcb5c53b8b36b91e
monkyyy <crazymonkyyy@gmail.com>
2023-12-12 10:44:26 -0700

    left draw in a partial working state wrote a todo and moving on

--- monkyyylib/monkyyydraw.d
+++ monkyyylib/monkyyydraw.d
@@ -10,7 +10,7 @@ void drawA(T...)(T args)=>draw(args,color:allcolor);
 //void draw(T...)(Vector2 vec2,T args)=>draw(x:cast(int)vec2.x,y:cast(int)vec2.y,args);
 struct gradent{}
 import toggle;
-void draw(gradent g,alias A,int low,size_t high,T...)(stickyindex!(A,low,high) color,T args)=>draw(color1:color++,color2:color--,args);
+//void draw(gradent g,alias A,int low,size_t high,T...)(stickyindex!(A,low,high) color,T args)=>draw(color1:color++,color2:color--,args);
 
 alias draw=raylib.DrawCircle;
 alias draw=raylib.DrawCircleGradient;
@@ -19,3 +19,11 @@ alias draw=raylib.DrawRectangle;
 alias draw=raylib.DrawRectangleGradientH;
 //alias draw=
 //alias draw=
+/*
+TODO:
+figure out how named agruments work with overload sets and expand this to allot of functions
+
+right now im getting nonsense errors, and shit
+
+I stuggle with 3 layers of recersion 
+*/
