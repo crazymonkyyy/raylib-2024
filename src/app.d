61dc312e03a2757580af38083f77a22eea2b29af
monkyyy <crazymonkyyy@gmail.com>
2024-02-15 08:01:00 -0700

    frut gam3 sorta coming together

--- src/app.d
+++ src/app.d
@@ -1,7 +1,7 @@
 import basic;
 import tempmath;
 mixin mainhack!();
-enum smallest=2.75;
+enum smallest=8;
 enum incrment=5;
 enum left=200;
 enum right=750;
@@ -14,12 +14,14 @@ struct ball{
 	ubyte color_;
 	Color color()=>monkyyycolor.color[color_];
 	int fren=-1;
-	void update(){
-		p.x+=v.x;
-		p.y+=v.y;
-		v.x*=.95;
-		v.y*=.95;
-		v.y+=.2;
+	void update(float step,bool friction)(){
+		p.x+=v.x*step;
+		p.y+=v.y*step;
+		static if(friction){
+			v.x*=.95;
+			v.y*=.95;
+		}
+		v.y+=.2*step;
 		bound;
 	}
 	auto bound(){
@@ -34,27 +36,36 @@ struct ball{
 		auto f=b?&DrawCircle:&DrawCircleLines;
 		f(cast(int)p.x,cast(int)p.y,size,color);
 	}
-	void collide()(ref ball o){
+	void collide(float step)(ref ball o){
+		if(CheckCollisionCircles(p,max(size,o.size),o.p,1)){//completely inside
+			if(color_<o.color_){
+				p.y-=o.size;
+				v.y=0;
+			} else {
+				o.p.y-=size;
+				o.v.y=0;
+			}
+		}
 		if(CheckCollisionCircles(p,size,o.p,o.size)){
 			auto x=p.x-o.p.x;
 			auto y=p.y-o.p.y;
 			auto n=Vector2(x,y).norm;
-			if(n.y>.2&&v.y<-.2){v.y*=.5;}
-			if(n.y<.2&&o.v.y>.2){o.v.y*=.5;}
-			p.x+=n.x*.45;
-			p.y+=n.y*.45;
-			o.p.x-=n.x*.45;
-			o.p.y-=n.y*.45;
+			if(n.y>.2&&v.y<-.2){v.y*=.5*step;}
+			if(n.y<.2&&o.v.y>.2){o.v.y*=.5*step;}
+			p.x+=n.x*.45*step;
+			p.y+=n.y*.45*step;
+			o.p.x-=n.x*.45*step;
+			o.p.y-=n.y*.45*step;
 			if(dis(p,o.p)<size/4){
-				p.x+=1;
-				p.y-=1;
-				n.x*=5;
-				n.y*=5;
+				p.x+=1*step;
+				p.y-=1*step;
+				n.x*=5*step;
+				n.y*=5*step;
 			}
-			v.x+=n.x*.1;
-			v.y+=n.y*.1;
-			o.v.x-=n.x*.1;
-			o.v.y-=n.y*.1;
+			v.x+=n.x*.1*step;
+			v.y+=n.y*.1*step;
+			o.v.x-=n.x*.1*step;
+			o.v.y-=n.y*.1*step;
 		}
 	}
 }
@@ -70,9 +81,11 @@ bool allsame(T)(T a, T b, T c) {
 
 nullablearray!(ball,1000) balls;
 void combine(int a,int b){
+	if(balls[a].color_!=balls[b].color_){return;}
 	bool touch(int a,int b){
 		if(a==-1 || b==-1){return false;}
-		return CheckCollisionCircles(balls[a].p,balls[a].size*1.5,balls[b].p,balls[b].size*1.5);
+		if(balls[a].color_!=balls[a].color_){return false;}
+		return CheckCollisionCircles(balls[a].p,balls[a].size*1.1,balls[b].p,balls[b].size*1.1);
 	}
 	if( ! touch(a,b)){return;}
 	int c;
@@ -84,15 +97,16 @@ void combine(int a,int b){
 		c=balls[b].fren;
 		goto match;
 	}
+	cleanup:
 	balls[a].fren=b;
 	balls[b].fren=a;
 	return;
 	match:
-		if( ! uniq(a,b,c)) return;
-		if(balls.nulls[a]){return;}
-		if(balls.nulls[b]){return;}
-		if(balls.nulls[c]){return;}
-		if( ! allsame(balls[a].size,balls[b].size,balls[c].size)){return;}
+		if( ! uniq(a,b,c)) goto cleanup;
+		if(balls.nulls[a]){goto cleanup;}
+		if(balls.nulls[b]){goto cleanup;}
+		if(balls.nulls[c]){goto cleanup;}
+		if( ! allsame(balls[a].size,balls[b].size,balls[c].size)){goto cleanup;}
 		balls~=ball(Vector2(
 				(balls[a].p.x+balls[b].p.x+balls[c].p.x)/3,
 				max(balls[a].p.y,balls[b].p.y,balls[c].p.y)),
@@ -108,26 +122,57 @@ void main_(){
 	makewindow;
 	next.v=Vector2(0,10);
 	next.color_=3;
+	auto logo=LoadTexture("assets/logo.png");
 	while (!WindowShouldClose()){
 		startdrawing;
+		DrawTextureEx(logo,Vector2(0,0),0,.25,Colors.WHITE);
 		drawH(left,top,right-left,bot-top);
 		foreach(i,_;balls){
 			foreach(j,__;balls){
 				if(j>=i)break;
 				combine(cast(int)i,cast(int)j);
-				balls[i].collide(balls[j]);
-			}
-			balls[i].update;
+		}}
+		foreach(i,_;balls){
 			balls[i].draw;
+		}
+		foreach(___;0..10){
+			if(___==0){
+				foreach(i,_;balls){
+					balls[i].update!(.1,true);
+				}
+			}else{
+				foreach(i,_;balls){
+					balls[i].update!(.1,false);
+				}
+			}
+			foreach(i,_;balls){
+				foreach(j,__;balls){
+					if(j>=i)break;
+					//combine(cast(int)i,cast(int)j);
+					balls[i].collide!.1(balls[j]);
+				}
+				//balls[i].update;
+				//balls[i].draw;
+			}
 		}
 		next.p=GetMousePosition;
 		next.bound;
 		next.draw(false);
 		with(button){
 		if(mouse1.pressed ||mouse1.ramp>30){
+			loop:
 			balls~=next;
-			next.color_=cast(ubyte)min(GetRandomValue(0,8),GetRandomValue(0,4));
+			next.color_=cast(ubyte)min(GetRandomValue(0,12),GetRandomValue(0,6));
+			//next.color_=cast(ubyte)GetRandomValue(0,24);
+			//if(space.toggle &&GetRandomValue(0,10000)!=0){
+			//	goto loop;
+			//}
 		}
+		if(f3){
+			foreach(i,_;balls){
+				if(_.fren!=-1)
+				DrawLineV(_.p,balls[_.fren].p,allcolors[7]);
+		}}
 		enddrawing;
 	}}
 	CloseWindow();
